c4f57b6198964caa848b8a67195d0d698abb8db3
Alec Dewulf <alecdewulf@Alecs-MacBook-Air.local>
2022-04-09 16:22:10 -0400

    more compliments

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ require("dotenv").config();
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-const compliments = ["Your hair doesn't look as bad as usual today", 
-"Being short is not as bad as you think it is", "Having friends is overrated"]
+const compliments = ["Your hair doesn't look as bad as usual today",
+    "Being short is not as bad as you think it is", "Having friends is overrated",
+    "Maggie is kind of cool sometimes", "Today is going to be a great day",
+    "You have really nice teeth", "Your breath doesn't smell as bad as it usually does",
+    "You don't look nearly as badly as normal after you shower",
+    "Maggie is kind of decent at baking"];
 
 client.on('ready', () => {
     console.log(`Logged in...`);
